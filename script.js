fa04b7cd6380bf8c95578c0514a0392a279ef2bb
Edozie <edoziechukwuma@gmail.com>
2022-09-25 06:56:43 -0600

    Add books sorted

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ addBtn.addEventListener('click', (e) => {
 e.preventDefault();
 const book = {
   title: title.value,
-  author: author.value
+  author: author.value,
 }
 bookArray.push(book);
+addBooks()
 });
+
+const addBooks = () => {
+    bookList.innerHTML = bookArray.map((data, index) => `<li class="book">
+    <p id="title">${data.title}</p>
+    <p id="author">${data.author}</p>
+    <button onclick=class="removeButton">Remove</button>
+    </li>`).join('');
+};
